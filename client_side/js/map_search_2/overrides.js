e37411e50499cfca299a9cfa03875da7979b389c
James Olson <james@booj.com>
2013-10-29 16:17:42 -0600

    added override js file for map search 2

---
+++ client_side/js/map_search_2/overrides.js
@@ -0,0 +1,21 @@
+/*global jQuery:true, _:true, Backbone:true, App:true*/
+
+;(function ($, _, bb, App) {
+	/**
+	 * Quick Docs
+	 *
+	 * Override a method in anything: 
+	 * App.<Views/Models/Collections>.<Module>.prototype.<Method> = function() {
+	 *      // overrides
+	 * };
+	 * Example: App.Views.AppView.prototype.createMap = function() {
+	 *      console.log('do something different');
+	 * }
+	 *
+	 * YOU WOULD PUT THIS FILE IN THE CLIENT'S APP FOLDER AND DO THE OVERRIDES THERE.
+	 *
+	 * ==============================================================================
+	 * DO NOT DO ANY OVERRIDES IN THIS FILE. IT'S JUST A PLACEHOLDER.
+	 * ==============================================================================
+	 */
+}(jQuery, _, Backbone, App));
